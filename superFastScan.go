d30d383240cf66baf069ca81c9910aab24b6b42d
KamilCzajczyk <61652376+KamilCzajczyk@users.noreply.github.com>
2025-02-27 14:39:43 +0100

    Add files via upload

---
+++ superFastScan.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	for i := 1; i <= 65535; i++ {
+		wg.Add(1)
+		go func(j int) {
+			defer wg.Done()
+			ip := "192.168.50.1" //!!!! Change this to the IP you want to scan
+			address := fmt.Sprintf("%s:%d", ip, j)
+			conn, err := net.Dial("tcp", address)
+			if err != nil {
+				return
+			}
+			conn.Close()
+			fmt.Printf("%d open\n", j)
+		}(i)
+	}
+	wg.Wait()
+}
